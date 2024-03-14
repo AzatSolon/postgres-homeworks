5edb263bffbaf21396438376ada57d814940d01d
Yamete Kudasai <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHz9UP48vsX5QHtRbnWSJfttNWrsUcnSHPgtuXt7kPlq your_email@example.com -ssh>
2024-03-14 13:29:13 +0300

    add reader and main

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,36 @@
+import psycopg2
+from config import read_csv
+
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+customer_list = []
+customer_rows = read_csv("north_data/customers_data.csv")
+for r in list(customer_rows):
+    customer_rows_tuple = (str(r[0]), str(r[1]), str(r[2]))
+    customer_list.append(customer_rows_tuple)
+
+employee_list = []
+employee_rows = read_csv("north_data/employees_data.csv")
+for r in list(employee_rows):
+    employee_rows_tuple = (int(r[0]), str(r[1]), str(r[2]), str(r[3]), str(r[4]), str(r[5]))
+    employee_list.append(employee_rows_tuple)
+
+orders_list = []
+order_rows = read_csv("north_data/orders_data.csv")
+for r in list(order_rows):
+    order_rows_tuple = (int(r[0]), str(r[1]), int(r[2]), str(r[3]), str(r[4]))
+    orders_list.append(order_rows_tuple)
+
+conn_params = psycopg2.connect(host="localhost",
+                               database="north",
+                               user="postgres",
+                               password="Azat22111"
+                               )
+try:
+    with conn_params:
+        with conn_params.cursor() as cur:
+            cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", customer_list)
+            cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employee_list)
+            cur.executemany("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", orders_list)
+finally:
+    conn_params.close()
